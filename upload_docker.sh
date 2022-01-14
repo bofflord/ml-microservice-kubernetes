c353ec185b0470205d911b0f28159e1985ff0923
boffmasta <boffinet@web.de>
2022-01-14 08:59:09 +0100

    completed task 4 upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=boffmasta/ml-app:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+#docker login -u boffmasta
+cat dockertoken.txt | docker login --username boffmasta --password-stdin
+docker image tag ml-app-local:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
